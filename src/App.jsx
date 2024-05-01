8480e031893ea0ed39cd6d94ca7d0fa0d40c407e
AmadeuGMeniconi <amadmeni@gmail.com>
2024-05-01 02:00:14 +0100

    Content component correction

--- src/App.jsx
+++ src/App.jsx
@@ -41,9 +41,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content />
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
